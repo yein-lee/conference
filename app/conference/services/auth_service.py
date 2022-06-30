f47da4e398a791992a1fa22bfc26a1b43bee56a8
yein-lee <agop0310@gmail.com>
2022-06-30 16:12:38 +0900

    feat/security config AuthService로 이동

--- app/conference/services/auth_service.py
+++ app/conference/services/auth_service.py
@@ -10,7 +10,6 @@ from utils.http_exceptions import \
      JWTDecodeException,
      ValidationException)
 from config.settings import Settings
-from config.security import ALGORITHM, pwd_context
 from conference.models.user_model import UserModel
 from conference.schemas.token_schema import Token, TokenData
 from conference.repositories.user_repo import UserRepo
@@ -22,11 +21,11 @@ class AuthService:
 
     @classmethod
     def verify_password(cls, plain_password: str, hashed_password: str) -> bool:
-        return pwd_context.verify(plain_password, hashed_password)
+        return cls.pwd_context.verify(plain_password, hashed_password)
 
     @classmethod
     def get_password_hash(cls, password: str) -> str:
-        return pwd_context.hash(password)
+        return cls.pwd_context.hash(password)
 
     @classmethod
     async def authenticate(cls, username: str, password: str) -> Optional[UserModel]:
@@ -44,14 +43,14 @@ class AuthService:
         access_token_expires = timedelta(minutes=Settings().ACCESS_TOKEN_EXPIRE_MINUTES)
         expire = datetime.utcnow() + access_token_expires
         to_encode = {"exp": expire, "sub": user_model.username}
-        access_token = jwt.encode(to_encode, Settings().SECRET_KEY, algorithm=ALGORITHM)
+        access_token = jwt.encode(to_encode, Settings().SECRET_KEY, algorithm=cls.ALGORITHM)
         token = Token(access_token=access_token, token_type="bearer")
         return token
 
     @classmethod
     def get_username_from_token(cls, token: str) -> str:
         try:
-            decoded_jwt = jwt.decode(token, Settings().SECRET_KEY, algorithms=[ALGORITHM])
+            decoded_jwt = jwt.decode(token, Settings().SECRET_KEY, algorithms=[cls.ALGORITHM])
             token_data = TokenData(**decoded_jwt)
             username = token_data.sub
         except JWTError:
